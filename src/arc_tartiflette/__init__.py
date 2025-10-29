ae757de1bae1d1559a391a42a6fd8996e42560d8
Méo Desbois-Renaudin <meo.desbois@gmail.com>
2025-10-29 20:05:10 +0100

    bug fix: import error unsloth

--- src/arc_tartiflette/__init__.py
+++ src/arc_tartiflette/__init__.py
@@ -1,5 +1,9 @@
 from dotenv import load_dotenv
+import os
 load_dotenv()
+
+# os.environ["TOKENIZERS_PARALLELISM"] = "false"
+os.environ["UNSLOTH_IS_PRESENT"] = "1"
 
 def main() -> None:
     print("Hello from arc-tartiflette!")
